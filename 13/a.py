af3f4006d432ad77fb8ac53c77d73be358492ee8
semvis123 <semvisscher10@gmail.com>
2022-12-13 16:48:19 +0100

    add day 13

---
+++ 13/a.py
@@ -0,0 +1,34 @@
+from inp import inputData, testData
+
+def a_smaller_than_b(a, b):
+    if isinstance(a, int) and isinstance(b, int):
+        return 1 if a < b else (-1 if a > b else 0)
+    if isinstance(a, list) and isinstance(b, list):
+        for i in range(len(a)): 
+            if i >= len(b):
+                return -1
+            result = a_smaller_than_b(a[i], b[i])
+            if result != 0:
+                return result
+        if len(a) < len(b):
+            return 1
+        return 0
+    if isinstance(a, int):
+        return a_smaller_than_b([a], b)
+    return a_smaller_than_b(a, [b])
+
+print(sum(i+1 for i, pair in enumerate(inputData) if a_smaller_than_b(*pair) == 1))
+
+# for pair in inputData:
+#     print(pair[0], pair[1])
+#     print(a_smaller_than_b(pair[0], pair[1]))
+    
+assert a_smaller_than_b(*testData[0]) == 1, testData[0]
+assert a_smaller_than_b(*testData[1]) == 1, testData[1]
+assert a_smaller_than_b(*testData[2]) == -1, testData[2]
+assert a_smaller_than_b(*testData[3]) == 1, testData[3]
+assert a_smaller_than_b(*testData[4]) == -1, testData[4]
+assert a_smaller_than_b(*testData[5]) == 1, testData[5]
+assert a_smaller_than_b(*testData[6]) == -1, testData[6]
+assert a_smaller_than_b(*testData[7]) == -1, testData[7]
+
