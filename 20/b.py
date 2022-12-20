2638c0c6f28cad70471451cc62ce6090bdf8aeb2
semvis123 <semvisscher10@gmail.com>
2022-12-20 15:40:31 +0100

    finish 20

---
+++ 20/b.py
@@ -0,0 +1,26 @@
+orig_file = []
+
+for line in open(0):
+    orig_file.append(int(line) * 811589153)
+
+new_file_order = [x for x in range(len(orig_file))]
+
+for _ in range(10):
+    for i, n in enumerate(orig_file):
+        curr = new_file_order.index(i)
+        new_file_order.pop(curr)
+        new_index = (curr + n - 1) % len(new_file_order) + 1
+        new_file_order.insert(new_index, i)
+
+new_file = [0] * len(new_file_order)
+for x, y in enumerate(new_file_order):
+    new_file[x] = orig_file[y]
+print(new_file)
+
+zero = new_file.index(0)
+coords = [zero] * 3
+coords[0] = new_file[(zero + 1000) % len(new_file)]
+coords[1] = new_file[(zero + 2000) % len(new_file)]
+coords[2] = new_file[(zero + 3000) % len(new_file)]
+
+print(sum(coords))
